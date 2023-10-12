be993dd56c0b3d68742f65a0d294195d7a480b65
KingSiong <kingsiong@163.com>
2023-10-12 08:24:37 +0800

    prefix-function

---
+++ Template/template.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <cmath>
+#include <iomanip>
+#include <cstring>
+#include <algorithm>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
