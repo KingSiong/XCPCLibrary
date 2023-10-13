23149b282079165cda2af96e0715ab9396035db3
KingSiong <kingsiong@163.com>
2023-10-13 08:23:10 +0800

    add suffix-array.hpp

--- StringTheory/Z-function.hpp
+++ StringTheory/Z-function.hpp
@@ -9,7 +9,7 @@ z[0] = 0
 vector<int> z_fun(const string &s) {
     int n = s.size();
     vector<int> z(n);
-    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+    for (int i = 1, l = 0, r = 0; i < n; i++) {
         int k =
             (i <= r && z[i - l] < (r - i + 1)) ? z[i - l] : max(0, r - i + 1);
         while (i + k < n && s[k] == s[i + k]) ++k;
