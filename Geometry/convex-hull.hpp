d78892884804dfed4e731fc06f2d07cae713b9ff
KingSiong <kingsiong@163.com>
2023-11-02 23:33:11 +0800

    change varibles

--- Geometry/convex-hull.hpp
+++ Geometry/convex-hull.hpp
@@ -2,19 +2,19 @@
 
 #include "./geometry-template.hpp"
 
-vector<Point> convex_hull(vector<Point> p) {
-    sort(p.begin(), p.end());
-    vector<Point> h(p.size() + 1);
-    h[0] = p[0];
+vector<Point> convex_hull(vector<Point> ps) {
+    sort(ps.begin(), ps.end());
+    vector<Point> h(ps.size() + 1);
+    h[0] = ps[0];
     int top = 1;
-    for (int i = 1; i < p.size(); ++i) {
-        while (top > 1 && cross(h[top - 1] - h[top - 2], p[i] - h[top - 1]) <= 0) --top;
-        h[top++] = p[i];
+    for (int i = 1; i < ps.size(); ++i) {
+        while (top > 1 && cross(h[top - 1] - h[top - 2], ps[i] - h[top - 1]) <= 0) --top;
+        h[top++] = ps[i];
     }
     int down = top;
-    for (int i = p.size() - 2; i; --i) {
-        while (top > down && cross(h[top - 1] - h[top - 2], p[i] - h[top - 1]) <= 0) --top;
-        h[top++] = p[i];
+    for (int i = ps.size() - 2; i; --i) {
+        while (top > down && cross(h[top - 1] - h[top - 2], ps[i] - h[top - 1]) <= 0) --top;
+        h[top++] = ps[i];
     }
     return {h.begin(), h.begin() + top};
 }
