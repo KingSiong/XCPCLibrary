717ae7359d7ac5f71820fea0063169137a595641
KingSiong <kingsiong@163.com>
2023-11-02 22:38:04 +0800

    add some comments

--- Geometry/side-of.hpp
+++ Geometry/side-of.hpp
@@ -2,6 +2,7 @@
 
 #include "./geometry-template.hpp"
 
+// left: 1; right: -1, on line: 0
 int side_of(const Point &s, const Point &e, const Point &p) {
     double a = cross(e - s, p - s), l = dist(e - s) * EPS;
     return (a > l) - (a < -l);
